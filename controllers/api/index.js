49e5d506717688b71f87ef329939fb949fe84fd8
Nicolas Fraenkel <nicfraenkel@gmail.com>
2022-12-20 16:18:00 +1030

    Completed Users model, Completed connection.js,

---
+++ controllers/api/index.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+
+
+const homeRoutes = require('./home-routes');
+
+module.exports = router;
---
+++ modals/Users.js
@@ -0,0 +1,52 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Users extends Model { }
+
+Users.init(
+    {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8]
+            }
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        }
+    },
+    {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'users'
+    }
+);
+
+module.exports = Users;
