961d993fa5553e435e7a6204c7484a3f5f668ebc
Nicolas Fraenkel <nicfraenkel@gmail.com>
2022-12-22 18:56:29 +1030

    Completed posts model

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,5 +1,13 @@
 const router = require('express').Router();
 
 //render homepage
+router.get('/', (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 
 module.exports = router;
---
+++ modals/Posts.js
@@ -0,0 +1,46 @@
+// model for posts
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Posts extends Model { }
+
+Posts.init(
+    {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.UUID,
+            references: {
+                model: 'users',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'posts'
+    }
+);
+
+    
+
+
