933c2d1e7ce45df9cbcb91fc63ac0f9defadc3f9
Nicolas Fraenkel <nicfraenkel@gmail.com>
2022-12-22 19:54:27 +1030

    added more seed data and temporarily removed hooks for bycrypt so that data can be seeded' '

--- models/Users.js
+++ models/Users.js
@@ -28,16 +28,16 @@ Users.init(
         }
     },
     {
-        hooks: {
-            async beforeCreate(newUserData) {
-                newUserData.password = await bcrypt.hash(newUserData.password, 10);
-                return newUserData;
-            },
-            async beforeUpdate(updatedUserData) {
-                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-                return updatedUserData;
-            }
-        },
+        // hooks: {
+        //     async beforeCreate(newUserData) {
+        //         newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        //         return newUserData;
+        //     },
+        //     async beforeUpdate(updatedUserData) {
+        //         updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        //         return updatedUserData;
+        //     }
+        // },
 
         sequelize,
         timestamps: false,
