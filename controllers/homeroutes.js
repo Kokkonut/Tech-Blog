77ea20ae4f579f2ad8f781d8bdb55e03ea43e2c2
Nicolas Fraenkel <nicfraenkel@gmail.com>
2022-12-22 20:26:22 +1030

    refactored handlbars

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -15,11 +15,12 @@ router.get('/', async (req, res) => {
 
         // Serialize data so the template can read it
         const posts = postData.map((post) => post.get({ plain: true }));
+        console.log(posts);
 
         // Pass serialized data and session flag into template
         res.render('homepage', {
-            // posts,
-            logged_in: req.session.logged_in
+            posts,
+            // logged_in: req.session.logged_in
         });
     } catch (err) {
         res.status(500).json(err);
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const homeRoutes = require('./homeroutes');
+const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
 
